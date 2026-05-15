0408b92ad4d56ff989e1264d995f647a5f1ac83b
Kavitha Arakutti Anandan <kavia1992@gmail.com>
2026-05-15 20:49:46 +0000

    changes to the code

---
+++ models/staging/stg_expenses.sql
@@ -0,0 +1,24 @@
+with ranked_claims AS (
+    select 
+        claim_id, 
+        employee_id, 
+        claim_date, 
+        expense_type,  
+        claimed_amount, 
+        currency, 
+        approval_status,
+        approver_id,
+        row_number() over(partition by claim_id order by claim_date desc ) as rn
+from raw.public.expense_claims
+)
+select  
+        claim_id, 
+        employee_id, 
+        claim_date, 
+        UPPER(expense_type) AS expense_type,  
+        claimed_amount, 
+        currency, 
+        LOWER(approval_status) AS approval_status,
+        approver_id
+from ranked_claims
+where rn =1;
